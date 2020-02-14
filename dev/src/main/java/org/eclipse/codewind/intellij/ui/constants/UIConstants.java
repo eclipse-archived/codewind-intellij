514dfac9893f48f102682baa1e5bb08e0e802953
Keith Chong <kchong@ca.ibm.com>
2020-02-14 08:52:59 -0500

    [IntelliJ] Update getting started link (#50)
    Signed-off-by: Keith Chong <kchong@ca.ibm.com>

--- dev/src/main/java/org/eclipse/codewind/intellij/ui/constants/UIConstants.java
+++ dev/src/main/java/org/eclipse/codewind/intellij/ui/constants/UIConstants.java
@@ -14,7 +14,7 @@ public class UIConstants {
     // Documentation links
     public static final String
             DOC_BASE_URL = "https://www.eclipse.org/codewind",
-            GETTING_STARTED_INFO_URL = DOC_BASE_URL + "/gettingstarted.html",
+            GETTING_STARTED_INFO_URL = DOC_BASE_URL + "/mdt-intellij-getting-started.md.html",
             CWSETTINGS_INFO_URL = DOC_BASE_URL + "/mdteclipsemanagingprojects.html",
             TEMPLATES_INFO_URL = DOC_BASE_URL + "/mdteclipseworkingwithtemplates.html",
             REGISTRY_INFO_URL = DOC_BASE_URL + "/image-registry-credentials.html",
