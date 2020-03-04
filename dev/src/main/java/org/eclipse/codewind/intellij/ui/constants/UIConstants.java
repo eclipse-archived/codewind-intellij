556ffbc361260824ff77b00c40c798dc730c4498
Joseph Kim <25185698+josephca@users.noreply.github.com>
2020-03-04 11:21:56 -0500

    Issue #2372:  fix broken doc link for IntelliJ 0.9.0 (#75)
    * Fix broken html file urls for 0.9.0

--- dev/src/main/java/org/eclipse/codewind/intellij/ui/constants/UIConstants.java
+++ dev/src/main/java/org/eclipse/codewind/intellij/ui/constants/UIConstants.java
@@ -13,11 +13,10 @@ package org.eclipse.codewind.intellij.ui.constants;
 public class UIConstants {
 
     // Documentation links
-    public static final String
-            DOC_BASE_URL = "https://www.eclipse.org/codewind",
-            GETTING_STARTED_INFO_URL = DOC_BASE_URL + "/mdt-intellij-getting-started.md.html",
+    public static final String DOC_BASE_URL = "https://www.eclipse.org/codewind",
+            GETTING_STARTED_INFO_URL = DOC_BASE_URL + "/mdt-intellij-getting-started.html",
             CWSETTINGS_INFO_URL = DOC_BASE_URL + "/mdteclipsemanagingprojects.html",
-            TEMPLATES_INFO_URL = DOC_BASE_URL + "/mdteclipseworkingwithtemplates.html",
+            TEMPLATES_INFO_URL = DOC_BASE_URL + "/workingwithtemplates.html",
             REGISTRY_INFO_URL = DOC_BASE_URL + "/image-registry-credentials.html",
-            REMOTE_SETUP_URL =  DOC_BASE_URL +"/remoteconfiguring.html";
+            REMOTE_SETUP_URL = DOC_BASE_URL + "/remoteconfiguring.html";
 }
